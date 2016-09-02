dfc5a31b4e1cb63c38c2932fa9b8f1fe0f935838
Matthew Miller <mnmiller1@me.com>
2016-09-02 19:59:18 +1000

    Removed bad code. Fixes #7

--- src/tests/integration/sanityTest.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-const describe = require('mocha').describe;
-const it = require('mocha').it;
-
-describe('Sanity check', () => {
-  it('Knows true is true', () => {
-    expect(true).to.equal(true);
-  });
-});
