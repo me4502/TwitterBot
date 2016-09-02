5560ca3473d0fa3fdfca90b605292504529074d4
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-09-02 16:40:06 +1000

    Wrong web url

--- src/client/js/home/index.js
+++ src/client/js/home/index.js
@@ -1,17 +1,5 @@
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:8080');
-
-function generateWS(){
-  let method;
-  if(location.host.startsWith('localhost') || location.host.startsWith('127.0.0.1')){
-    method = 'ws://';
-  } else {
-    method = 'wss://';
-  }
-  return method + location.host;
-};
-
-const socketURI = generateWS();
+const socket = io.connect(location.host);
 
 socket.on('news', function (data) {
   console.log(data); // eslint-disable-line
