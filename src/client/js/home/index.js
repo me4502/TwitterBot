31ae6f028f295386c4966eda1e15f08da603525f
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-09-02 16:37:46 +1000

    Generate WS on fly

--- src/client/js/home/index.js
+++ src/client/js/home/index.js
@@ -1,6 +1,18 @@
 const io = require('socket.io-client');
 const socket = io.connect('http://localhost:8080');
 
+function generateWS(){
+  let method;
+  if(location.host.startsWith('localhost') || location.host.startsWith('127.0.0.1')){
+    method = 'ws://';
+  } else {
+    method = 'wss://';
+  }
+  return method + location.host;
+};
+
+const socketURI = generateWS();
+
 socket.on('news', function (data) {
   console.log(data); // eslint-disable-line
   socket.emit('my other event', { my: 'data' });
